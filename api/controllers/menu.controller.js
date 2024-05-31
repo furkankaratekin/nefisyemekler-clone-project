b2d875795e0691fd95d36d7288be2f4c9e318fd5
Alperen Aktuğ <aktugalperen9@gmail.com>
2024-05-31 16:53:31 +0300

    api-menus-added

---
+++ api/controllers/menu.controller.js
@@ -0,0 +1,47 @@
+import Menu from "../models/menu.model.js";
+import { errorHandler } from "../utils/error.js";
+
+//Tüm menüleri getir
+export const getAllMenus = async (req, res) => {
+  try {
+    const menus = await Menu.find();
+    res.json(menus);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Belirli bir menüyü id'ye göre getir
+export const getMenuById = async (req, res) => {
+  try {
+    // URL'den id parametresini alın
+    const { id } = req.params;
+
+    // Doğrudan id değerini kullanarak menüyü bulun
+    const menu = await Menu.findOne({ _id: id });
+
+    // Menü bulunamazsa, 404 hatası döndür
+    if (!menu) {
+      return res.status(404).json({ message: "Menu not found" });
+    }
+
+    // Menü bulunursa, menüyü JSON formatında döndür
+    res.json(menu);
+  } catch (error) {
+    // Hata oluşursa, 500 hatası ile hata mesajını döndür
+    res.status(500).json({ message: error.message });
+  }
+};
+
+//Popüler menüler
+export const getPopularMenus = async (req, res) => {
+  try {
+    // popular alanı true olan menüleri sorgula
+    const popularMenus = await Menu.find({ popular: true });
+    res.status(200).json(popularMenus);
+  } catch (error) {
+    errorHandler(res, error.message, 500);
+  }
+};
+
+//Popüler menüleri getir(boolean değerine göre)
---
+++ api/routes/menu.routes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { getAllMenus, getPopularMenus,getMenuById } from "../controllers/menu.controller.js";
+ 
+const router = express.Router();
+
+ router.get("/",getAllMenus);
+ router.get("/popular",getPopularMenus);
+router.get("/:id", getMenuById); // Belirli bir menüyü id'ye göre getir
+
+
+//Tüm menüleri listele
+//Tüm popüler menüleri listele
+
+export default router;
